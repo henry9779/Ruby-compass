16d62390223d6959c761712d0d6def82e4c9b02e
Henry <vinsen1109@gmail.com>
2021-03-11 04:29:22 +0800

    test done

--- quiz.rb
+++ quiz.rb
@@ -1,4 +1,4 @@
-# Prompts the user for two integers.
+# Prompts the user for two integesteps.
 # - The first one should be between 1 and 4, with
 #   * 1 meaning initially looking North,
 #   * 2 meaning initially looking East,
@@ -20,7 +20,7 @@
 
 begin
   # 將箭頭用 hash 存起來，之後用 key 得出箭頭
-  unicode = {
+  routes = {
     'N' => '↑',
     'NE' => '↗',
     'E' => '→',
@@ -51,31 +51,31 @@ begin
     case
       when base_3 == 0
         puts ""
-        puts "Ok, you want to first look this way: #{unicode['N']}"
+        puts "Ok, you want to first look this way: #{routes['N']}"
         puts ""
         puts "In base 3, the second input reads as: #{base_3}"
-        puts "So that's how you want to go: #{unicode['N']}"
+        puts "So that's how you want to go: #{routes['N']}"
         puts ""
         puts "Let's go then!"
-        puts unicode['N']
+        puts routes['N']
       when base_3 == 1
         puts ""
-        puts "Ok, you want to first look this way: #{unicode['N']}"
+        puts "Ok, you want to first look this way: #{routes['N']}"
         puts ""
         puts "In base 3, the second input reads as: #{base_3}"
-        puts "So that's how you want to go: #{unicode['WN']}"
+        puts "So that's how you want to go: #{routes['WN']}"
         puts ""
         puts "Let's go then!"
-        puts unicode['WN']
+        puts routes['WN']
       when base_3 == 2
         puts ""
-        puts "Ok, you want to first look this way: #{unicode['N']}"
+        puts "Ok, you want to first look this way: #{routes['N']}"
         puts ""
         puts "In base 3, the second input reads as: #{base_3}"
-        puts "So that's how you want to go: #{unicode['NE']}"
+        puts "So that's how you want to go: #{routes['NE']}"
         puts ""
         puts "Let's go then!"
-        puts unicode['NE']
+        puts routes['NE']
     end
   end
 
@@ -83,26 +83,26 @@ begin
     case
       when base_3 == 0
         puts ""
-        puts "Ok, you want to first look this way: #{unicode['E']}"
+        puts "Ok, you want to first look this way: #{routes['E']}"
         puts ""
         puts "In base 3, the second input reads as: #{base_3}"
-        puts "So that's how you want to go: #{unicode['E']}"
+        puts "So that's how you want to go: #{routes['E']}"
         puts ""
         puts "I don't want to have the sun in my eyes, but by all means have a go at it!"
       when base_3 == 1
         puts ""
-        puts "Ok, you want to first look this way: #{unicode['E']}"
+        puts "Ok, you want to first look this way: #{routes['E']}"
         puts ""
         puts "In base 3, the second input reads as: #{base_3}"
-        puts "So that's how you want to go: #{unicode['NE']}"
+        puts "So that's how you want to go: #{routes['NE']}"
         puts ""
         puts "I don't want to have the sun in my eyes, but by all means have a go at it!"
       when base_3 == 2
         puts ""
-        puts "Ok, you want to first look this way: #{unicode['E']}"
+        puts "Ok, you want to first look this way: #{routes['E']}"
         puts ""
         puts "In base 3, the second input reads as: #{base_3}"
-        puts "So that's how you want to go: #{unicode['ES']}"
+        puts "So that's how you want to go: #{routes['ES']}"
         puts ""
         puts "I don't want to have the sun in my eyes, but by all means have a go at it!"
     end
@@ -112,31 +112,31 @@ begin
     case
       when base_3 == 0
         puts ""
-        puts "Ok, you want to first look this way: #{unicode['S']}"
+        puts "Ok, you want to first look this way: #{routes['S']}"
         puts ""
         puts "In base 3, the second input reads as: #{base_3}"
-        puts "So that's how you want to go: #{unicode['S']}"
+        puts "So that's how you want to go: #{routes['S']}"
         puts ""
         puts "Let's go then!"
-        puts unicode['S']
+        puts routes['S']
       when base_3 == 1
         puts ""
-        puts "Ok, you want to first look this way: #{unicode['S']}"
+        puts "Ok, you want to first look this way: #{routes['S']}"
         puts ""
         puts "In base 3, the second input reads as: #{base_3}"
-        puts "So that's how you want to go: #{unicode['ES']}"
+        puts "So that's how you want to go: #{routes['ES']}"
         puts ""
         puts "Let's go then!"
-        puts unicode['ES']
+        puts routes['ES']
       when base_3 == 2
         puts ""
-        puts "Ok, you want to first look this way: #{unicode['S']}"
+        puts "Ok, you want to first look this way: #{routes['S']}"
         puts ""
         puts "In base 3, the second input reads as: #{base_3}"
-        puts "So that's how you want to go: #{unicode['SW']}"
+        puts "So that's how you want to go: #{routes['SW']}"
         puts ""
         puts "Let's go then!"
-        puts unicode['SW']
+        puts routes['SW']
     end
   end
 
@@ -144,63 +144,80 @@ begin
     case
       when base_3 == 0
         puts ""
-        puts "Ok, you want to first look this way: #{unicode['W']}"
+        puts "Ok, you want to first look this way: #{routes['W']}"
         puts ""
         puts "In base 3, the second input reads as: #{base_3}"
-        puts "So that's how you want to go: #{unicode['W']}"
+        puts "So that's how you want to go: #{routes['W']}"
         puts ""
         puts "I don't want to have the sun in my eyes, but by all means have a go at it!"
       when base_3 == 1
         puts ""
-        puts "Ok, you want to first look this way: #{unicode['W']}"
+        puts "Ok, you want to first look this way: #{routes['W']}"
         puts ""
         puts "In base 3, the second input reads as: #{base_3}"
-        puts "So that's how you want to go: #{unicode['SW']}"
+        puts "So that's how you want to go: #{routes['SW']}"
         puts ""
         puts "I don't want to have the sun in my eyes, but by all means have a go at it!"
       when base_3 == 2
         puts ""
-        puts "Ok, you want to first look this way: #{unicode['W']}"
+        puts "Ok, you want to first look this way: #{routes['W']}"
         puts ""
         puts "In base 3, the second input reads as: #{base_3}"
-        puts "So that's how you want to go: #{unicode['WN']}"
+        puts "So that's how you want to go: #{routes['WN']}"
         puts ""
         puts "I don't want to have the sun in my eyes, but by all means have a go at it!"
     end
   end
 
-  base_3_arr = base_3.to_s.split('').to_a
+  base_3_arr = base_3.to_s.split('')
+  steps = []
   i = 0
-  rs = []
+
   # 判斷 base_3 為多個數且陣列數量大於2時
   if [1].include?(initial_direction) && base_3_arr.size > 2
-    puts "Ok, you want to first look this way: #{unicode['N']}"
-    puts ""
-    puts "In base 3, the second input reads as: #{base_3}"
-    puts "So that's how you want to go: #{rs.join}"
-    puts ""
-    puts "Let's go then!"
     base_3_arr.each do |n|
       case
       when n == '0'
-        arr = []
-        (i).times { arr.push(" ")}
-        arr.push(unicode['N'])
-        puts arr.join
+        steps.push(routes['N'])
 
       when n == '1'
-        arr = []
-        (i + 1).times { arr.push(" ")}
-        arr.push(unicode['WN'])
-        puts arr.join
-        i += 1
+        steps.push(routes['WN'])
 
       when n == '2'
-        arr = []
-        (i + 1).times { arr.push(" ")}
-        arr.push(unicode['NE'])
-        puts arr.join
-        i -= 1
+        steps.push(routes['NE'])
+      end
+    end
+
+    puts "Ok, you want to first look this way: #{routes['N']}"
+    puts ""
+    puts "In base 3, the second input reads as: #{base_3}"
+    puts "So that's how you want to go: #{steps.join}"
+    puts ""
+    puts "Let's go then!"
+
+    i = (steps.count('↖') - steps.count('↗')).abs
+
+    base_3_arr.reverse!.each do |n|
+      case
+      when n == '0'
+      arr = []
+      (i).times { arr << (" ") }
+      arr << routes['N']
+      puts arr.join
+
+      when n == '1'
+      arr = []
+      (i + 1).times { arr << (" ") }
+      arr << routes['WN']
+      puts arr.join
+      i += 1
+
+      when n == '2'
+      arr = []
+      (i - 1).times { arr << (" ") }
+      arr << routes['NE']
+      puts arr.join
+      i -= 1
       end
     end
   end
@@ -209,51 +226,66 @@ begin
     base_3_arr.each do |n|
       case
       when n == '0'
-        rs << unicode['E']
+        steps << routes['E']
       when n == '1'
-        rs << unicode['NE']
+        steps << routes['NE']
       when n == '2'
-        rs << unicode['ES']
+        steps << routes['ES']
       end
     end
-    puts "Ok, you want to first look this way: #{unicode['E']}"
+    puts "Ok, you want to first look this way: #{routes['E']}"
     puts ""
     puts "In base 3, the second input reads as: #{base_3}"
-    puts "So that's how you want to go: #{rs.join}"
+    puts "So that's how you want to go: #{steps.join}"
     puts ""
     puts "I don't want to have the sun in my eyes, but by all means have a go at it!"
   end
 
   if [3].include?(initial_direction) && base_3_arr.size > 2
-    puts "Ok, you want to first look this way: #{unicode['S']}"
-    puts ""
-    puts "In base 3, the second input reads as: #{base_3}"
-    puts "So that's how you want to go: #{rs.join}"
-    puts "Let's go then!"
     base_3_arr.each do |n|
       case
       when n == '0'
-        arr = []
-        (i).times { arr.push(" ")}
-        arr.push(unicode['S'])
-        puts arr.join
+        steps << routes['S']
 
       when n == '1'
-        arr = []
-        (i + 1).times { arr.push(" ")}
-        arr.push(unicode['ES'])
-        puts arr.join
-        i += 1
+        steps << routes['ES']
 
       when n == '2'
-        arr = []
-        (i + 1).times { arr.push(" ")}
-        arr.push(unicode['SW'])
-        puts arr.join
-        i += 1
+        steps << routes['SW']
+
       end
-      # 先抓出最小值
-      # 如果最小值為負數歸零
+    end
+
+    puts "Ok, you want to first look this way: #{routes['S']}"
+    puts ""
+    puts "In base 3, the second input reads as: #{base_3}"
+    puts "So that's how you want to go: #{steps.join}"
+    puts "Let's go then!"
+
+    i = (steps.count('↙')-steps.count('↘')).abs
+
+    base_3_arr.each do |n|
+      case
+      when n == '0'
+      arr = []
+      (i).times { arr << (" ") }
+      arr << routes['S']
+      puts arr.join
+
+      when n == '1'
+      arr = []
+      (i + 1).times { arr << (" ") }
+      arr << routes['ES']
+      puts arr.join
+      i += 1
+
+      when n == '2'
+      arr = []
+      (i - 1).times { arr << (" ") }
+      arr << routes['SW']
+      puts arr.join
+      i -= 1
+      end
     end
   end
 
@@ -261,19 +293,19 @@ begin
     base_3_arr.each do |n|
       case
       when n == '0'
-        rs << unicode['W']
+        steps << routes['W']
       when n == '1'
-        rs << unicode['SW']
+        steps << routes['SW']
       when n == '2'
-        rs << unicode['WN']
+        steps << routes['WN']
       end
     end
-      puts "Ok, you want to first look this way: #{unicode['W']}"
-      puts ""
-      puts "In base 3, the second input reads as: #{base_3}"
-      puts "So that's how you want to go: #{rs.join}"
-      puts ""
-      puts "I don't want to have the sun in my eyes, but by all means have a go at it!"
+    puts "Ok, you want to first look this way: #{routes['W']}"
+    puts ""
+    puts "In base 3, the second input reads as: #{base_3}"
+    puts "So that's how you want to go: #{steps.join}"
+    puts ""
+    puts "I don't want to have the sun in my eyes, but by all means have a go at it!"
   end
 
 rescue ArgumentError => e
